9f8508514cbc6ad74f2c52003dd3b95029d22058
david <daviddecastro42@live.co.uk>
2019-12-31 02:36:10 +0000

    finished all weather page used weather API

--- src/components/WeatherContainer.js
+++ src/components/WeatherContainer.js
@@ -1,14 +1,61 @@
 import React, { useState } from 'react';
 import '../Styles/Weather.css';
+import WeatherInfo from './WeatherInfo'
 
 
 function WeatherConatiner(){
-    const [searchQuery, setSearchQuery] = useState();
+    const API_KEY = '979fe8cb6d6bc10f32721d06957faff6';
+    const [searchQuery, setSearchQuery] = useState('');
+    const  [weatherData, setWeatherData] = useState({
+        temp: null,
+        humidity: null,
+        desc: null,
+        city: null
+    })
+
+    const [isValidZipCode, setIsValidZipCode] = useState(true);
 
     function updateSearchQuery(event){
-        setSearchQuery(event.target.value);
+        let zipCode = event.target.value
+        let isValid = validateZipCode(zipCode);
+        setSearchQuery(zipCode);
+        
+        if(isValid || zipCode === "" || isValid.length === 5){
+            
+            setIsValidZipCode(true);
+        } else{
+            setIsValidZipCode(false);
+
+        }
 
     }
+
+    function validateZipCode(zipCode){
+        let regex= /[0-9]{5}/;
+        return regex.test(zipCode);
+    }
+
+    function getWeatherData(){
+        if(!isValidZipCode || searchQuery === ""){
+            setIsValidZipCode(false)
+            return;
+        }
+
+        fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${searchQuery},us&appid=${API_KEY}`)
+        .then(response => response.json())
+        .then(data => setWeatherData({
+            temp: convertToFarenheit(data.main.temp),
+            humidity: data.main.humidity,
+            desc: data.weather[0].main,
+            city: data.name
+        }))
+
+    }
+
+    function convertToFarenheit(temp){
+        return  ((temp - 273.15) * (9.0 / 5.0) + 32).toFixed(0)
+    }
+
     return(
         <section className ="weather-container">
            <header className="weather-header">
@@ -18,10 +65,21 @@ function WeatherConatiner(){
                        placeholder="Zip Code"
                        className="search-input"
                        onChange={updateSearchQuery}
+                       maxLength='5'
                    />
-                   <button className="material-icons">search</button>
+                   <button onClick={getWeatherData} className="material-icons">search</button>
                </div>
            </header>
+           <p className="error"> {isValidZipCode ? '' : 'Invalid Zip Code'}</p>
+           <section className="weather-info">
+
+           {weatherData.temp === null ? (
+               <p>No Weather to Display<i className="material-icons">wb_sunny</i></p>
+
+           ) : <WeatherInfo  data={weatherData}/>
+           }
+
+           </section>
 
         </section>
     )
